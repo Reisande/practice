da87cc2ed28e295289453f4cf3e1024694466943
Reisande <mryabk2@uic.edu>
2019-09-08 12:27:26 -0500

    Finished basic implementation of vector based stack, has not been tested yet

--- stack.cpp
+++ stack.cpp
@@ -71,16 +71,56 @@ class VStack {
 private:
 	std::vector<int> stack;
 	int length;
+	int capacity;
 
 public:
 	VStack() {
 		length = 0;
+		capacity = 10;
 	}
-	void push(int val);
-	int pop();
-	int top();
-	int size();
-	bool isEmpty();
+
+	void push(int val) {
+		stack[length] = val;
+
+		length++;
+	}
+
+	int pop() {
+		if(length == 0) {
+			std::cout << "Stack is empty, nothing to pop\n";
+			return -1;
+		}
+		else {
+			length--;
+
+			int returnValue = queue[length];
+			
+			if(length < capacity / 2 && capacity > 10) {
+				capacity /= 2;
+				queue.resize(capacity);
+			}
+
+			return returnValue;
+		}
+	}
+
+	int top() {
+		if(length == 0) {
+			std::cout << "Stack is empty, nothing to pop\n";
+			return -1;
+		}
+		else {
+			return queue[length - 1];
+		}
+	}
+
+	int size() {
+		return length;
+	}
+
+	bool isEmpty() {
+		return (length == 0);
+	}
 	
 };
 
